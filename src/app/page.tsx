87b81332ef82ba47089de4e46cab2d54d131c33a
ryutaro-kimura <sasuker2264@gmail.com>
2025-08-18 08:47:56 +0900

    refactor: ♻️ ホームページのリダイレクト処理を改善
    ホームページからプレースページへのリダイレクトを直接実装しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-    </div>
+    redirect('/places')
   );
 }
